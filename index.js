5219bf918851bc6e0769664a2b2af6e922fdac05
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-21 10:31:57 -0300

    terminado boxplot

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ csv(require("./data/boxplots.csv")).then((data) => {
 });
 
 function boxplot(incomingData) {
-  let maximoMedian = max(incomingData, (d) => {
-    return d.median;
-  });
-
   let maximoDay = max(incomingData, (d) => {
     return d.day;
   });
+
   let yScale = scaleLinear().domain([0, 100]).range([470, 20]);
-  let xScale = scaleLinear().domain([1, maximoDay]).range([0, 470]);
-  let yAxis = axisLeft(yScale).tickSize(-470);
+  let xScale = scaleLinear().domain([0, maximoDay]).range([0, 470]);
+
+  let yAxis = axisLeft(yScale).tickSize(-520).tickPadding(7);
   let xAxis = axisBottom(xScale)
-    .tickValues([1, 2, 3, 4, 5, 6, 7])
-    .tickSize(-470);
+    .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
+    .tickSize(-470)
+    .tickPadding(7);
 
   select("svg")
     .append("g")
@@ -46,6 +45,54 @@ function boxplot(incomingData) {
       return "translate(" + (xScale(d.day) + 20) + "," + yScale(d.median) + ")";
     })
     .each(function (d) {
-      select(this).append("circle").attr("r", 10).style("fill", "darkgray");
+    //   select(this).append("circle").attr("r", 10).style("fill", "darkgray");
+
+      select(this)
+        .append("line")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", yScale(d.max) - yScale(d.median))
+        .attr("y2", yScale(d.min) - yScale(d.median))
+        .style("stroke", "#457b9d")
+        .style("stroke-width", "4px");
+
+      select(this)
+        .append("line")
+        .attr("class", "max")
+        .attr("x1", -10)
+        .attr("x2", 10)
+        .attr("y1", yScale(d.max) - yScale(d.median))
+        .attr("y2", yScale(d.max) - yScale(d.median))
+        .style("stroke", "#457b9d")
+        .style("stroke-width", "4px");
+
+      select(this)
+        .append("line")
+        .attr("class", "min")
+        .attr("x1", -10)
+        .attr("x2", 10)
+        .attr("y1", yScale(d.min) - yScale(d.median))
+        .attr("y2", yScale(d.min) - yScale(d.median))
+        .style("stroke", "#457b9d")
+        .style("stroke-width", "4px");
+
+      select(this)
+        .append("rect")
+        .attr("class", "distribution")
+        .attr("height", yScale(d.q1) - yScale(d.q3))
+        .attr("x", -10)
+        .attr("y", yScale(d.q3) - yScale(d.median))
+        .attr("width", 20)
+        .style("fill", "#f1faee")
+        .style("stroke", "black")
+        .style("stroke-width", "1px");
+
+      select(this)
+        .append("line")
+        .attr("class", "median")
+        .attr("x1", -10)
+        .attr("x2", 10)
+        .style("stroke", "#e63946")
+        .style("stroke-width", "4px");
     });
 }
